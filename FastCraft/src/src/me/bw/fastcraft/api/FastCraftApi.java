79e1bb7fcbbe4483e99ab5eee29167119e0b6725
BuildTools <unconfigured@null.spigotmc.org>
2015-08-24 14:03:18 -0400

    Revert "FastCraft v19"
    This reverts commit 8b4995105400cd5806abe9b38d6ff97d75c34ea6.

--- FastCraft/src/src/me/bw/fastcraft/api/FastCraftApi.java
+++
@@ -1,8 +0,0 @@
-package me.bw.fastcraft.api;
-
-/**
- * @deprecated as of v16. API moved to co.kepler.api.FastCraftApi.
- */
-@Deprecated
-public class FastCraftApi extends co.kepler.fastcraft.api.FastCraftApi {
-}
